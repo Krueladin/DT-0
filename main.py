66fc03c4a38e472cf69fc00070638bdfef46e4b9
MathMonsterX <trudosm@plu.edu>
2017-06-29 10:35:14 -0700

    Add Docstrings

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ import math
 import csv
 
 def calc_entropy(target):
+    """Calculates the entropy of a single column.
+    
+    Entropy is the measure of uncertainty within data.
+    H(S) = Sum (x in X, 1, k): -p(x)*log(p(x), k)
+    Where 
+        H(S) - The measure of uncertainty in a dataset 'S'.
+        S - The current dataset ebing operated on.
+        X - Set of classes in 'S'.
+        x - A class of 'S'.
+        k - The number of classes within 'S'. Also used to normalize dataset entropy to a value 
+            between 0 and 1 within the log portion of the calculation.
+        p(x) - The proportion of the occurance of 'x' in the dataset 'S'.
+    target - An array of data representing a single column of a dataset.
+
+    Returns the calculated entropy of the passed in column.
+    """
     classes = find_label_totals(target)
     tot_classes = len(classes)
     if tot_classes <= 1:
         return 0
 
-#    print classes
     # Calculate the total sum of values.
     tot_sum = 0
     for key in classes:
@@ -21,6 +36,26 @@ def calc_entropy(target):
     return entropy
 
 def calc_info_gain(col, target):
+    """Calculates the information gain according to a target column (column with desired values
+    for prediction) and a column of data from the same dataset.
+    Used for training a descision tree.
+
+    Information Gain is a measure of the difference of entropy after the dataset 'S' is split
+    on an attribute (column) of the dataset 'A'.
+    G(A, S) = H(S) - Sum (t in T, 1, k): p(t)H(t)
+    Where
+        G(A, S) - Information gain of dataset 'S'.
+        S - The current dataset being operated on.
+        A - An attribute (data from the same label for multiple records) of the dataset 'S'.
+        T - The subsets creating by splitting S by attribute A s.t. each split (new subset 't')
+            has a homogenous 'a' (unique value in 'A').
+        p(t) - The proportion of the number of elements in 't' to the number of elements in 'S'.
+        H(t) - Entropy of subset 't'.
+    col - An array of data representing a single column of a dataset.
+    target - An array of data representing the information desired in classification.
+
+    Returns a float value representing the information gain of the parameter 'col'.
+    """
     # Find the classes within the target column
     # and the entropy of the target column.
     t_classes = find_label_totals(target)
@@ -37,12 +72,13 @@ def calc_info_gain(col, target):
         if len(comb_data) <= 0:
             c_entropies.append(0.0)
             continue
-        # Calculate the weight for this separation.
+        # Calculate the weight, p(t) for this separation.
         weight = float(len(comb_data)) / len(col)
         # Add the entropy of the column class to the entropies array.
         c_entropies.append(weight * calc_entropy(comb_data))
     
     # Calculate the average entropy of the column.
+    #   The sum of each p(t)H(t).
     avg_entropy = 0.0
     for item in c_entropies:
         avg_entropy += item
@@ -50,6 +86,13 @@ def calc_info_gain(col, target):
     return t_entropy - avg_entropy
 
 def find_label_totals(col):
+    """Takes a data column and finds each unique value.
+
+    col - An array of data representing a single column of a dataset.
+
+    Returns a map with key for each unique element in the array 'col' passed in as a parameter.
+        The values for this map indicate the frequency of appearance of each unique value.
+    """
     val_dict = {}
     for k in col:
         if k not in val_dict:
@@ -76,8 +119,6 @@ def main():
     # Select the column giving the most information gain.
     target = cols[0]
     del cols[0]
-#    print target
-#    print cols
     col_gain = []
     for col in cols:
         col_gain.append(calc_info_gain(col, target))
